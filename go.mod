4553e63cbb4d6b01dcddfb46fa688f9b82132732
FedoraTipper <19745176+FedoraTipper@users.noreply.github.com>
2021-08-21 21:16:57 +0200

    Transformer: Change parameter type to new MinerSeries type

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.16
 
 require (
 	github.com/go-redis/redis/v8 v8.11.3
+	github.com/spf13/viper v1.8.1
+	github.com/spf13/pflag v1.0.5
 	golang.org/x/crypto v0.0.0-20210813211128-0a44fdfbc16e
 )
--- internal/transformer/transformer.go
+++ internal/transformer/transformer.go
@@ -11,9 +11,9 @@ type Transformer interface {
 	ConvertStatsPayloadToMiner(string, string, interface{}) (*models.Miner, error)
 }
 
-func GetTransformer(model string) (Transformer, error) {
+func GetTransformer(model constants.MinerSeries) (Transformer, error) {
 	switch model {
-	case constants.X19_MinerSeries:
+	case constants.X19:
 		return &S19Transformer{}, nil
 	default:
 		return nil, errors.New(fmt.Sprintf("Unknown model: %s", model))
